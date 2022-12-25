c191316fa0b6d682302bd94d6c84ce4c3f14b613
sanycoosh <sanycoosh@mail.ru>
2022-12-25 15:04:34 +0300

    Решение задач по теме "Переменные,ввод/вывод,условия,циклы"

---
+++ task_3.py
@@ -0,0 +1,5 @@
+print('Вычисление суммы ряда чисел от 1 до 100')
+sum = 0
+for i in range(1,101):
+    sum = sum+i
+print(sum)
---
+++ task_4.py
@@ -0,0 +1,5 @@
+print('Вычисление произведения ряда чисел от 1 до 10')
+proizvedenie = 1
+for i in range(1,11):
+    proizvedenie = proizvedenie * i
+print(proizvedenie)
